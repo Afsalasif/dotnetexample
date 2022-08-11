ec5a3c034854e2dd8f60658665e5b1470e47e788
Afsalasif <afsalasif138@gmail.com>
2022-08-11 17:44:07 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("The current DAY is " + DateTime.Now.ToString("dddd")+" and The current MONTH is " + DateTime.Now.ToString("MMMM"));
+
